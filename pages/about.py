6e99d3170d95f408b3d891545553a2faaff41681
EmperorSiloso <salsabilaariny15@gmail.com>
2026-05-27 16:21:42 +0700

    Add files via upload

---
+++ pages/about.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("About This Project")
+
+st.write("""
+This project is an AI-powered MCYT lore assistant built with:
+
+- Streamlit
+- Google Gemini API
+- JSON databases
+- Love and passion!!
+- Visual Studio Code
+
+The chatbot answers questions about different SMP universes and characters using lore and wiki information!!
+""")
